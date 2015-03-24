39a1c7a7ba3b803a00c4c237fdd7ad2656a93c73
Chris Luhring <cluhring@gmail.com>
2015-03-24 10:47:20 -0600

    Address Migration

--- db/migrate/20150324164123_create_users.rb
+++ db/migrate/20150324164123_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :nickname
+      t.string :first_name
+      t.string :last_name
+      t.string :full_name
       t.string :email
-      t.string :provder
+      t.string :provider
       t.string :token
       t.string :uid
       t.string :image_url
