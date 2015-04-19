450d871b60561f0db2ac07502c5f64640cf7e0ca
Chris Luhring <cluhring@gmail.com>
2015-04-19 12:02:57 -0600

    why

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,8 @@ class WelcomeController < ApplicationController
 
   def create
     @location = Location.create(address: params[:address])
-    session[:latitude1] = @location.latitude
-    session[:longitude1] = @location.longitude
+    session[:latitude1] = @location.latitude || 39.749747
+    session[:longitude1] = @location.longitude || -104.999977
     redirect_to stations_path
   end
 
